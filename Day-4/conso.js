545f3fd511b3d485163c61db8932bedd2f341033
Mayuresh Gage <mayureshgage84@gmail.com>
2023-03-25 22:50:05 +0530

    Day-4 : console module

---
+++ Day-4/conso.js
@@ -0,0 +1,8 @@
+console.log('Hello world');
+console.log("Hello universe");
+
+let num = 45;
+let str = 'This is string';
+
+console.log('this is a number',num);
+console.log('5. %s','this is a my string');
