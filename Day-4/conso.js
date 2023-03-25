73c9328fb80ee50b0a112fab492c13c3c22c1fc1
Mayuresh Gage <mayureshgage84@gmail.com>
2023-03-25 23:53:13 +0530

    day-4 addition

--- Day-4/conso.js
+++ Day-4/conso.js
@@ -6,3 +6,8 @@ let str = 'This is string';
 
 console.log('this is a number',num);
 console.log('5. %s','this is a my string');
+
+// clear a console
+// console.clear();
+console.count('hello');
+console.count('hello');
